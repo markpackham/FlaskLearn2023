02e76b82453aae85d92097086f89635468c466d6
mark <markpackham1@gmail.com>
2023-02-17 11:33:52 +0000

    test flask can run
    https://youtu.be/Qr4QMBUPxWo?t=763

--- market.py
+++ market.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World'
+    return "<h1>Hello, World!</h1>"
+
+# has to run via
+# flask --app .\market.py run
