8adb8f55036cedca2695df249014e164f79e540d
mark <markpackham1@gmail.com>
2023-02-23 10:39:00 +0000

    demo use to double route for homepage
    https://youtu.be/Qr4QMBUPxWo?t=2017

--- market.py
+++ market.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def home_page():
-    return render_template('home.html')
-
-
 # make sure to turn "Debug mode" ON so code changes are updated
 # rather than constantly restarting the server
 # set FLASK_DEBUG=1
@@ -16,3 +11,13 @@ def home_page():
 # @app.route('/about/<username>')
 # def about_page(username):
 #     return f"<h1>This is the about page of {username}</h1>"
+
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+
+
+
+
