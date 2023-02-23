aa9a415a3ced270492f3f923240feaa57e4296d1
mark <markpackham1@gmail.com>
2023-02-23 09:49:19 +0000

    demo a dynamic route
    https://youtu.be/Qr4QMBUPxWo?t=1248

--- market.py
+++ market.py
@@ -2,12 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
 
 # make sure to turn "Debug mode" ON so code changes are updated
 # rather than constantly restarting the server
 # set FLASK_DEBUG=1
 # has to run via
 # flask --app .\market.py run
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f"<h1>This is the about page of {username}</h1>"
