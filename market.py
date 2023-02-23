cadc188e9d3a3288d7f8c2ab27819ce3d88c10f7
mark <markpackham1@gmail.com>
2023-02-23 09:40:26 +0000

    comment how to enable debug mode for code change updates
    https://youtu.be/Qr4QMBUPxWo?t=984

--- market.py
+++ market.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
+# make sure to turn "Debug mode" ON so code changes are updated
+# rather than constantly restarting the server
+# set FLASK_DEBUG=1
 # has to run via
 # flask --app .\market.py run
