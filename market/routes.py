f66e42e1500abbc18b1a291b87ec1852d670df22
mark <markpackham1@gmail.com>
2023-02-24 21:04:09 +0000

    restructure market project
    https://youtu.be/Qr4QMBUPxWo?t=7488

--- market/routes.py
+++ market/routes.py
@@ -1,12 +1,11 @@
-# @app.route('/about/<username>')
-# def about_page(username):
-#     return f"<h1>This is the about page of {username}</h1>"
+from market import app
+from flask import render_template
+from market.models import Item
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template('home.html')
-
 
 
 @app.route('/market')
---
+++ run.py
@@ -0,0 +1,8 @@
+from market import app
+
+# check if run.py has executed
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# run python via
+# "python run.py"
