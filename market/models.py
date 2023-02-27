254c234ba9e9a66436fb5ceb060582c7f4d6ba5a
mark <markpackham1@gmail.com>
2023-02-27 09:35:50 +0000

    establish relationship between User and Item models
    https://youtu.be/Qr4QMBUPxWo?t=8166

--- market/models.py
+++ market/models.py
@@ -6,6 +6,9 @@ class User(db.Model):
     email_address = db.Column(db.String(length=255), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=255), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
+    # grab all items in one go so set "lazy" to True
+    # this won't be stored as a column but as a relationship
+    items = db.relationship('Item', backref='owned_user', lazy=True)
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -13,6 +16,8 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    # needed in order to have a relationship with User
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
