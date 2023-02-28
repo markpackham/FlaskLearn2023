7f82a9d961b249e775c71661dde6c2ed82d79d4c
mark <markpackham1@gmail.com>
2023-02-28 11:38:42 +0000

    validate username and password length
    https://youtu.be/Qr4QMBUPxWo?t=11091

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
+    username = StringField(label='User Name:', validators=Length(min=3, max=100))
     email_address = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:')
+    password1 = PasswordField(label='Password:', validators=Length(min=3))
     password2 = PasswordField(label='Confirm Password:')
     submit = SubmitField(label='Create Account')
