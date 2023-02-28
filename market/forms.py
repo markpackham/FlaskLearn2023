8212b169d87991c503e984770d1a4c9b30dfec00
mark <markpackham1@gmail.com>
2023-02-28 12:05:55 +0000

    add email validation module
    https://youtu.be/Qr4QMBUPxWo?t=11624

--- market/forms.py
+++ market/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:', validators=[Length(min=3, max=100), DataRequired])
-    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired])
-    password1 = PasswordField(label='Password:', validators=[Length(min=3), DataRequired])
-    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired])
+    username = StringField(label='User Name:', validators=[Length(min=3, max=100), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=3), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
