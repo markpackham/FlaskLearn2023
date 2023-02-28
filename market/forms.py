9cc4550e123483cc3f5dc4a248f80b350a49cb0d
mark <markpackham1@gmail.com>
2023-02-28 11:40:13 +0000

    check if password1 equals password2
    https://youtu.be/Qr4QMBUPxWo?t=11142

--- market/forms.py
+++ market/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length
+from wtforms.validators import Length, EqualTo
 
 
 class RegisterForm(FlaskForm):
     username = StringField(label='User Name:', validators=Length(min=3, max=100))
     email_address = StringField(label='Email Address:')
     password1 = PasswordField(label='Password:', validators=Length(min=3))
-    password2 = PasswordField(label='Confirm Password:')
+    password2 = PasswordField(label='Confirm Password:', validators=EqualTo('password1'))
     submit = SubmitField(label='Create Account')
