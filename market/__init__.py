36a9fad04bec5c6e8f36c0802e38b511ebe1d8a6
mark <markpackham1@gmail.com>
2023-03-01 09:40:19 +0000

    add flask login manger library
    https://youtu.be/Qr4QMBUPxWo?t=14493

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'b304cb55136e4ed30c6dac91'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
 from market import routes
