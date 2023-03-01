addd493dc3350af8b163c134486fa671fdddaf4a
mark <markpackham1@gmail.com>
2023-03-01 08:30:11 +0000

    remove unused import

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
